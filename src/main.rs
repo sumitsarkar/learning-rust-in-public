1e04d4d5ec6ad367bb00524704fa9cbe495b40d8
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2024-08-11 17:43:49 +0530

    First pass on deploying the application

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        SqlitePool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        SqlitePool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Sqlite.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection_pool)?.await
 }
