f3ff9c532c167d08ded273967feaab078bfe5721
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2024-08-12 11:31:54 +0530

    Add validations

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,76 @@
+use validator::ValidateEmail;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if s.validate_email() {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claims::{assert_err, assert_ok};
+    use fake::{
+        faker::internet::en::SafeEmail,
+        locales::{self, Data},
+        Fake,
+    };
+
+    use crate::domain::subscriber_email::SubscriberEmail;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let username = g
+                .choose(locales::EN::NAME_FIRST_NAME)
+                .unwrap()
+                .to_lowercase();
+            let domain = g.choose(&["com", "net", "org"]).unwrap();
+            let email = format!("{username}@example.{domain}");
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "potatotomato.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@tomato.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parse_successfully() {
+        let email = SafeEmail().fake();
+        assert_ok!(SubscriberEmail::parse(email));
+    }
+}
