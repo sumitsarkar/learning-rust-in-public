da516422ae8d5312504c30da7c8cbd21208e3f8f
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2024-08-12 22:58:49 +0530

    Refactoring tests further

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -4,23 +4,15 @@ use crate::helpers::spawn_app;
 
 #[sqlx::test]
 async fn subscribe_returns_a_200_for_valid_form_data(pool: SqlitePool) {
-    let test_app = spawn_app(pool).await;
-
-    let client = reqwest::Client::new();
+    let app = spawn_app(pool).await;
 
     let body = "name=Potato%20Tomato&email=potato%40tomato.com";
-    let response = client
-        .post(&format!("{}/subscriptions", &test_app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&test_app.db_pool)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
@@ -30,8 +22,7 @@ async fn subscribe_returns_a_200_for_valid_form_data(pool: SqlitePool) {
 
 #[sqlx::test]
 async fn subscribe_returns_a_400_when_data_is_missing(pool: SqlitePool) {
-    let test_app = spawn_app(pool).await;
-    let client = reqwest::Client::new();
+    let app = spawn_app(pool).await;
 
     let test_cases = vec![
         ("name=potato", "missing the email"),
@@ -40,13 +31,7 @@ async fn subscribe_returns_a_400_when_data_is_missing(pool: SqlitePool) {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = client
-            .post(&format!("{}/subscriptions", &test_app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscriptions(invalid_body.into()).await;
 
         assert_eq!(
             400,
@@ -60,7 +45,6 @@ async fn subscribe_returns_a_400_when_data_is_missing(pool: SqlitePool) {
 #[sqlx::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_empty(pool: SqlitePool) {
     let app = spawn_app(pool).await;
-    let client = reqwest::Client::new();
 
     let test_cases = vec![
         ("name=&email=potato%40gmail.com", "empty name"),
@@ -69,13 +53,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_empty(pool: SqliteP
     ];
 
     for (body, description) in test_cases {
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscriptions(body.into()).await;
 
         assert_eq!(
             400,
