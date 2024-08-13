d0c1c216460a736a0dda38f08db99d7e3c86603a
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2024-08-13 19:58:41 +0530

    Add name to trace

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -98,7 +98,7 @@ fn generate_subscription_token() -> String {
 }
 
 #[tracing::instrument {
-    name = "",
+    name = "Storing subscription token",
     skip(transaction, subscription_token)
 }]
 pub async fn store_token(
