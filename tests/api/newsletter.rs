bb6ae4b9c05944c06aea3a3f0f3f13770239244b
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2024-08-16 10:27:55 +0530

    write tests for authentication

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -125,3 +125,27 @@ async fn create_confirmed_subscriber(app: &TestApp) {
         .error_for_status()
         .unwrap();
 }
+
+#[sqlx::test]
+async fn requests_missing_authorization_are_rejected(pool: SqlitePool) {
+    let app = spawn_app(pool).await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter Title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
